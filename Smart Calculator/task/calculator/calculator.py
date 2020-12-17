a0988e127998f9b29849c5d684269093e8b4235f
supermads <madisonsfahey@gmail.com>
2020-12-17 14:52:56 -0800

    Stage 2 of Project

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,7 +1,20 @@
 # write your code here
-def add():
-    a, b = list(map(int, input().split()))
-    print(a + b)
+def add(nums):
+    if nums:
+        sum = 0
+        for n in nums:
+            sum += int(n)
+        print(sum)
 
 
-add()
+def main():
+    while True:
+        nums = input().split()
+        if nums == ['/exit']:
+            print('Bye')
+            return
+        else:
+            add(nums)
+
+
+main()
