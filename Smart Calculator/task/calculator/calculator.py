544f74a139da16c662eb4c357c9db1c383a26de4
supermads <madisonsfahey@gmail.com>
2020-12-17 16:26:20 -0800

    Stage 4 of Project

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,22 +1,49 @@
-# write your code here
+def process_expression(exp):
+    changes = {'++': '+', '--': '+', '+-': '-', '-+': '-'}
+    before_len = len(exp)
+    while True:
+        for original, replacement in changes.items():
+            while original in exp:
+                exp = exp.replace(original, replacement)
+        if len(exp) == before_len:
+            break
+        before_len = len(exp)
+    return exp
+
+
 def add(nums):
     if nums:
         acc = 0
         for n in nums:
             acc += int(n)
-        print(acc)
+        return acc
+
+
+def subtract(nums):
+    a, b = map(int, nums)
+    return a - b
 
 
 def main():
     while True:
-        nums = input().split()
-        if nums == ['/exit']:
+        exp = input()
+        if exp == '/exit':
             print('Bye')
             return
-        elif nums == ['/help']:
-            print('The program calculates the sum of numbers')
-        else:
-            add(nums)
+        elif exp == '/help':
+            print('The program calculates the result of given expressions')
+        elif exp:
+            exp = process_expression(exp)
+            sub_exps = exp.split()
+            acc = sub_exps[0]
+            while len(sub_exps) > 1:
+                if sub_exps[1] == '+':
+                    acc = add([sub_exps[0], sub_exps[2]])
+                elif sub_exps[1] == '-':
+                    acc = subtract([sub_exps[0], sub_exps[2]])
+                del sub_exps[0:2]
+                sub_exps[0] = acc
+            print(acc)
 
 
 main()
